7f783e5a2ab56e764b30529ac145d1de60c3b9cb
obezglavlen <mykyta.statiev@gmail.com>
2023-08-31 13:20:42 +0300

    clear

--- providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
-import { FC, PropsWithChildren } from "react";
-
-export const Providers: FC<PropsWithChildren> = (props) => {
-  const { children } = props;
-  return <SessionProvider>{children}</SessionProvider>;
-};
--- template.tsx
+++
@@ -1,8 +0,0 @@
-import { FC, PropsWithChildren } from "react";
-import styles from "./page.module.css";
-
-const Template: FC<PropsWithChildren> = ({ children }) => {
-  return <main className={styles.main}>{children}</main>;
-};
-
-export default Template;
